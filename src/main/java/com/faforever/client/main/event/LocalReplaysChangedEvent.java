0d929cf1e30d2d8c89667fc110954e58b76f33db
alexander <alexander.von.trostorff@gmail.com>
2020-07-16 19:12:40 +0200

    Alex refactor

--- src/main/java/com/faforever/client/main/event/LocalReplaysChangedEvent.java
+++ src/main/java/com/faforever/client/main/event/LocalReplaysChangedEvent.java
@@ -1,24 +1,14 @@
 package com.faforever.client.main.event;
 
 import com.faforever.client.replay.Replay;
-import lombok.Getter;
 import lombok.Value;
-import org.springframework.context.ApplicationEvent;
 
 import java.util.Collection;
 
-@Getter
-public class LocalReplaysChangedEvent extends ApplicationEvent {
-
-  private final Collection<Replay> newReplays;
-  private final Collection<Replay> deletedReplays;
-  private final boolean changedInFolder;
-
-  public LocalReplaysChangedEvent(Object source, Collection<Replay> newReplays, Collection<Replay> deletedReplays, boolean changedInFolder) {
-    super(source);
-    this.newReplays = newReplays;
-    this.deletedReplays = deletedReplays;
-    this.changedInFolder = changedInFolder;
-  }
+@Value
+public class LocalReplaysChangedEvent {
+  int page;
+  int totalPages;
+  Collection<Replay> replays;
 }
 
